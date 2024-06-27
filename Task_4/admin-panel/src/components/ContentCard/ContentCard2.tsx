dcbfe6b28f0c846b401c7e24a42bb1f19b7594b6
codal-uchoudhary <uchoudhary@codal.com>
2024-06-27 18:32:27 +0530

    CIT-410 #time 1h implement card container for the admin page

---
+++ Task_4/admin-panel/src/components/ContentCard/ContentCard2.tsx
@@ -0,0 +1,8 @@
+
+const ContentCard2=()=>{
+    return(
+        <div>Content card 2</div>
+    )
+}
+
+export default ContentCard2
---
+++ Task_4/admin-panel/src/components/ContentCard/ContentCard5.tsx
@@ -0,0 +1,8 @@
+
+const ContentCard5=()=>{
+    return(
+        <div>Content card 5</div>
+    )
+}
+
+export default ContentCard5
